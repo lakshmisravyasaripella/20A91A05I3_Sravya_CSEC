6b24fa044544d5ccfac6edaf152cf19aaa498c00
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-05-24 12:13:25 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,15 @@
+a=int(input("a"))
+b=int(input("b"))
+if a%2==0:
+    print("a is even")
+else:
+    print("a is odd")
+if b%2==0:
+    print("b is even")
+else:
+    print("b is odd")
+#Output:
+a2486962336555
+b1334684356595478
+a is odd
+b is even
