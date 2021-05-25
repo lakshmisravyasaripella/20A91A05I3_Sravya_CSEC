a2c87772177728fdf1b7e91b1b641961e8663469
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-05-25 14:26:20 +0530

    Add files via upload

---
+++ division.py
@@ -0,0 +1,18 @@
+num=int(input("Enter number"))
+if num%2==0:
+    print("It is divisible by 2")
+elif num%3==0:
+    print("It is divisible by 3")
+elif num%5==0:
+    print("It is divisible by 5")
+else:
+    print("It is not divisible by 2,3 and 5")
+#Output:
+Enter number22
+It is divisible by 2
+Enter number21
+It is divisible by 3
+Enter number55
+It is divisible by 5
+Enter number19
+It is not divisible by 2,3 and 5
