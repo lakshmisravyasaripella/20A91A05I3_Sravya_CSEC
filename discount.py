30fc4ee10a7ce823e0a86ab986cac6aee378d8bd
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-05-26 14:41:59 +0530

    Add files via upload

---
+++ discount.py
@@ -0,0 +1,25 @@
+amount=int(input("Enter amount"))
+if 1000<amount<2000:
+    discount=amount*(10/100)
+    print("Discount is",discount)
+    print("Total amount is",amount-discount)
+elif 2000<amount<3000:
+    discount=amount*(20/100)
+    print("Discount is",discount)
+    print("Total amount is",amount-discount)
+elif 3000<amount<5000:
+    discount=amount*(30/100)
+    print("Discount is",discount)
+    print("Total amount is",amount-discount)
+elif amount>5000:
+    discount=amount*(40/100)
+    print("Discount is",discount)
+    print("Total amount is",amount-discount)
+else:
+    print("There is no discount")
+    print("Total amount is",amount)
+#Output:
+Enter amount3800
+Discount is 1140.0
+Total amount is 2660.0     
+  
