b530270f521b5b9a6953b882f2af529da680909d
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-05-28 12:32:15 +0530

    Add files via upload

---
+++ while4.py
@@ -0,0 +1,11 @@
+i=int(input("Enter start value"))
+n=int(input("Enter end value"))
+s=int(input("Enter step count"))
+while i<=n:
+    print(i,end="\t")
+    i=i+s
+#Output:
+Enter start value1
+Enter end value9
+Enter step count2
+1	3	5	7	9    
