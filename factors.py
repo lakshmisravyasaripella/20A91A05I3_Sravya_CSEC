285a331051729d674ee98ac948456fee536850b5
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-05-31 12:45:51 +0530

    Add files via upload

---
+++ factors.py
@@ -0,0 +1,11 @@
+n=int(input("Enter number"))
+i=1
+print("The factors of n are:")
+while i<=n:
+    if n%i==0:
+        print(i,end=" ")
+    i=i+1
+#Output:
+Enter number18
+The factors of n are:
+1 2 3 6 9 18     
