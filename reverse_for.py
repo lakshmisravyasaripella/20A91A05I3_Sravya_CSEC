3dee0f4eb1536a2d43c64150e43c826f6f30da11
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-06-03 17:12:34 +0530

    Add files via upload

---
+++ reverse_for.py
@@ -0,0 +1,12 @@
+n=int(input("Enter number"))
+d=int(input("Enter the number of digits"))
+rev=0
+for i in range(0,d):
+    rem=n%10
+    rev=(rev*10)+rem
+    n=n//10
+print("The reverse of the given number is",rev)
+#Output:
+Enter number79898649
+Enter the number of digits8
+The reverse of the given number is 94689897
