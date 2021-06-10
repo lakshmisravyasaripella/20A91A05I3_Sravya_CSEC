0062715548a88cf10014a7ef5c96bb8cc4b931ce
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-06-10 14:19:53 +0530

    Update while3.py

--- while3.py
+++ while3.py
@@ -4,9 +4,10 @@ s=int(input("Enter step count"))
 while i<=n:
     print(i,end="\t")
     i=i+s
-#Output:
+'''    
+Output:
 Enter start value2
 Enter end value10
 Enter step count2
 2	4	6	8	10    
-
+'''
