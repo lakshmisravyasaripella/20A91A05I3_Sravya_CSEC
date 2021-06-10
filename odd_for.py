c4e1401aa406f80cc5fd8be64ca7a822d7944fe2
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-06-10 14:15:05 +0530

    Update odd_for.py

--- odd_for.py
+++ odd_for.py
@@ -2,7 +2,9 @@ s=int(input("Enter starting number"))
 e=int(input("Enter ending number"))
 for i in range(s,e+1,2):
     print(i,end=" ")
-#Output:
+'''    
+Output:
 Enter starting number1
 Enter ending number11
 1 3 5 7 9 11     
+'''
