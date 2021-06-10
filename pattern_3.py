9c61f31949fd0a4eb7c74d01225cfbbdf6e00f82
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-06-10 14:17:14 +0530

    Update pattern_3.py

--- pattern_3.py
+++ pattern_3.py
@@ -6,8 +6,9 @@ for i in range(1,n+1):
 for i in range(n,0,-1):
     for j in range(1,i+1):
         print(j,end=" ")
-    print("\n")        
-#Output:
+    print("\n")
+'''    
+Output:
 Enter number of rows5
 1 
 
@@ -28,4 +29,4 @@ Enter number of rows5
 1 2 
 
 1 
-    
+'''    
