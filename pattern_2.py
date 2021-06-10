119b4cb5f3d514ff1d5396439fc410eecc312b57
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-06-10 14:16:36 +0530

    Update pattern_2.py

--- pattern_2.py
+++ pattern_2.py
@@ -3,7 +3,8 @@ for i in range(n,0,-1):
     for j in range(1,i+1):
         print(j,end=" ")
     print("\n")
-#Output:    
+'''    
+Output:    
 Enter number of rows4
 1 2 3 4 
 
@@ -12,3 +13,4 @@ Enter number of rows4
 1 2 
 
 1     
+'''
