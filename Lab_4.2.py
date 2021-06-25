47b62eb0de91c11de92a5311cb81477f72e6ad46
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-06-25 12:01:15 +0530

    Add files via upload

---
+++ Lab_4.2.py
@@ -0,0 +1,16 @@
+a=input("Enter a string:")
+b=input("Enter a string:")
+if (len(a)==len(b)):
+    print("Both the strings are of equal length")
+else:
+    print("Both the strngs are not of equal length")
+'''
+Output:
+Enter a string:Lucky
+Enter a string:Vidya
+Both the strings are of equal length
+
+Enter a string:Sravya
+Enter a string:Harshitha
+Both the strngs are not of equal length
+'''
