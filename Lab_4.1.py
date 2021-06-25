32cc2195dab29ac712b0a75ae31693ea1c947a55
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-06-25 12:19:07 +0530

    Add files via upload

---
+++ Lab_4.1.py
@@ -0,0 +1,16 @@
+s=input("Enter a word:")
+print("Vowels in the word are:")
+for char in s:
+    if char=='a' or char=='e' or char=='i' or char=='o' or char=='u' or char=='A' or char=='E' or char=='I' or char=='O' or char=='U':
+        print(char,end=" ")
+'''
+Output:
+Enter a word:python
+Vowels in the word are:
+o 
+
+Enter a word:Aditya
+Vowels in the word are:
+A i a 
+'''
+   
