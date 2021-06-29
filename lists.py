9aad99fd27af5208d229ef1a211d20ac1507551a
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-06-29 14:53:23 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,16 @@
+list=[30,50,20,90,40]
+print(list)
+print(list[1])
+print(list[1:4])
+print(list[1:4:2])
+print("Maximum element of the list is",max(list))
+print("Minimum element of the list is",min(list))
+'''
+Output:
+[30, 50, 20, 90, 40]
+50
+[50, 20, 90]
+[50, 90]
+Maximum element of the list is 90
+Minimum element of the list is 20
+'''
