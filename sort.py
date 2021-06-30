e5f6a10919e924d24e223a06d7085da8efa0b18c
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-06-30 10:35:32 +0530

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,12 @@
+list=[30,60,50,20,40,10]
+print(list)
+list.sort()
+print(list)
+list.sort(reverse=True)
+print(list)
+'''
+Output:
+[30, 60, 50, 20, 40, 10]
+[10, 20, 30, 40, 50, 60]
+[60, 50, 40, 30, 20, 10]
+'''
