e6d9d0ca3cfe1a92d2b7a7ea95987b3e27907107
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-06-30 10:32:06 +0530

    Add files via upload

---
+++ append.py
@@ -0,0 +1,13 @@
+list=[20,38,56,75,45]
+print(list)
+list.append(899)
+list.append("Python")
+list.append(1990)
+print(list)
+print(type(list))
+'''
+Output:
+[20, 38, 56, 75, 45]
+[20, 38, 56, 75, 45, 899, 'Python', 1990]
+<class 'list'>
+'''
