ebe95f9f356e569a0098dd5eed8dc6b91891e23e
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-07-07 10:28:24 +0530

    Add files via upload

---
+++ set_sum.py
@@ -0,0 +1,12 @@
+set={11,33,24,56,78}
+print(set)
+print("Sum is",sum(set))
+print("Maximum integer from set is",max(set))
+print("Minimum integer from set is",min(set))
+'''
+Output:
+{33, 24, 56, 11, 78}
+Sum is 202
+Maximum integer from set is 78
+Minimum integer from set is 11
+'''
