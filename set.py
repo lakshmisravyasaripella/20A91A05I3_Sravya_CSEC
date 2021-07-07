fe3cb67469700dec5cb76a6794a2e8bd1ea13838
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-07-07 10:28:05 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,17 @@
+set_college={"AEC","ACET","ACOE"}
+print(set_college)
+print(type(set_college))
+print(len(set_college))
+set={"SLS","SRV","VVS","SRV"}
+print(set)
+print(type(set))
+print(len(set))
+'''
+Output:
+{'ACET', 'ACOE', 'AEC'}
+<class 'set'>
+3
+{'SLS', 'SRV', 'VVS'}
+<class 'set'>
+3
+'''
