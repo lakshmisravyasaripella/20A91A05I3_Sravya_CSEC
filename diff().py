7afdef6040e60cc69baf9c1d583bf1a5f43aaeb8
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-07-09 12:36:27 +0530

    Add files via upload

---
+++ diff().py
@@ -0,0 +1,13 @@
+s1={22,4,60,70,50}
+s2={33,67,90,70,50,22}
+print("Difference")
+print(s1.difference(s2))
+print("Symmetric difference")
+print(s1^s2)
+'''
+Output:
+Difference
+{4, 60}
+Symmetric difference
+{33, 67, 4, 90, 60}
+'''
