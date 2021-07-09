951e953a16dd65a5077d2d1b288f8f8894e01a7d
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-07-09 12:36:51 +0530

    Add files via upload

---
+++ isdisjoint().py
@@ -0,0 +1,15 @@
+s1={1,2,3,4,5,6}
+s2={10,20,30,40,50,60,70}
+print(s1.isdisjoint(s2))
+print(s2.isdisjoint(s1))
+s1={1,2,3,4,5,6,10}
+s2={10,20,30,40,50,60,70}
+print(s1.isdisjoint(s2))
+print(s2.isdisjoint(s1))
+'''
+Output:
+True
+True
+False
+False
+'''
